a68ae3a6b0d87dbacdc62ff2984e22805fe1e1fa
saikhanlayy <kokolynnsai.com>
2020-06-11 23:32:51 +0630

    Init commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/send-mail',function(){
+$details=[
+    'title'=>'Mail from MS IT',
+    'body'=>'This is from testing mail'
+];
+\Mail::to('mgmyint7850@gmail.com')->send(new \App\Mail\TestMail($details));
+echo "Email has been sent";
+});
+    
